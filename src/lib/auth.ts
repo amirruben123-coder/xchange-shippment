fcd0dc48fc5f40563a1bb14b8d6c5e2b131840b1
amirruben123-coder <amirruben123@gmail.com>
2026-06-24 09:00:11 -0700

    Update auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,7 +3,7 @@ import Credentials from 'next-auth/providers/credentials'
 import bcrypt from 'bcryptjs'
 import { prisma } from '@/lib/prisma'
 
-export const { handlers, auth, signIn, signOut } = NextAuth({
+export const { handlers, auth, signIn, signOut } = NextAuth({trusthost:true
   session: { strategy: 'jwt' },
   pages: { signIn: '/login' },
   callbacks: {
